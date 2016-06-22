660c9a2491a3acce07b40dcf4ca95c8a1ea53e4d
Josselin TD <josselin.td@gmail.com>
2016-06-22 23:03:12 +0000

    Add style to login/signup

--- client/js/app.js
+++ client/js/app.js
@@ -18,105 +18,6 @@ angular
 
 angular
     .module('bemyapp-tech-env')
-    .controller('NavigationController', NavigationController);
-
-function NavigationController($scope, $rootScope, Session) {
-    var ctrl = this;
-    
-    $scope.Session = Session;
-    
-    $rootScope.href = 'login';
-    $rootScope.id = '';
-    
-    ctrl.goTo = Session.goTo;
-    ctrl.grab = grab;
-    ctrl.logout = logout;
-    
-    $scope.$watch('Session.current', function() {
-        var current = Session.current.navigation;
-        
-        if (!Session.hasSession()) {
-            $rootScope.href = 'login';
-            return;
-        }
-        
-        if (!current) {
-            $rootScope.href = 'home';
-            return;
-        }
-        
-        if (current === 'whiteboard-list') {
-            $rootScope.href = 'whiteboard-list';
-            return;
-        }
-        
-        if (current === 'conference') {
-            $rootScope.href = 'conference';
-            return;
-        }
-        
-        if (current === 'videos-playlist') {
-            $rootScope.href = 'videos-playlist';
-            return;
-        }
-        
-        if (current === 'videos-player') {
-            $rootScope.href = 'videos-player';
-            return;
-        }
-        
-        if (current === 'videos-plalist') {
-            $rootScope.href = 'videos-plalist';
-            return;
-        }
-        
-        if (current === 'url' && Session.current.url) {
-            $rootScope.href = 'url';
-            $rootScope.url = Session.current.url;
-            return;
-        }
-        
-        if (current.indexOf('whiteboard') !== -1) {
-            $rootScope.id = current.split('/')[1];
-            $rootScope.href = 'whiteboard';
-            return;
-        }
-        
-        $rootScope.href = 'home';
-        return;
-    }, true);
-    
-    function grab(toGrab) {
-        var toGrab = angular.copy(Session.read({name: toGrab}));
-        if (toGrab) {
-            toGrab.url = Session.current.url;
-            toGrab.navigation = Session.current.navigation;
-            Session.update(toGrab);
-        }
-    }
-    
-    function logout() {
-        Session.unset();
-    }
-}
-/* global angular */
-
-angular
-    .module('bemyapp-tech-env')
-    .directive('navigation', NavigationDirective);
-
-function NavigationDirective() {
-    return {
-        restrict: 'E',
-        templateUrl: 'app/navigation/navigation.html',
-        controller: 'NavigationController',
-        controllerAs: 'navigationCtrl'
-    };
-}
-/* global angular */
-
-angular
-    .module('bemyapp-tech-env')
     .controller('ConferenceController', ConferenceController);
 
 function ConferenceController() {
@@ -218,6 +119,105 @@ function SessionService(Socket, $rootScope) {
     }
 }
 
+/* global angular */
+
+angular
+    .module('bemyapp-tech-env')
+    .controller('NavigationController', NavigationController);
+
+function NavigationController($scope, $rootScope, Session) {
+    var ctrl = this;
+    
+    $scope.Session = Session;
+    
+    $rootScope.href = 'login';
+    $rootScope.id = '';
+    
+    ctrl.goTo = Session.goTo;
+    ctrl.grab = grab;
+    ctrl.logout = logout;
+    
+    $scope.$watch('Session.current', function() {
+        var current = Session.current.navigation;
+        
+        if (!Session.hasSession()) {
+            $rootScope.href = 'login';
+            return;
+        }
+        
+        if (!current) {
+            $rootScope.href = 'home';
+            return;
+        }
+        
+        if (current === 'whiteboard-list') {
+            $rootScope.href = 'whiteboard-list';
+            return;
+        }
+        
+        if (current === 'conference') {
+            $rootScope.href = 'conference';
+            return;
+        }
+        
+        if (current === 'videos-playlist') {
+            $rootScope.href = 'videos-playlist';
+            return;
+        }
+        
+        if (current === 'videos-player') {
+            $rootScope.href = 'videos-player';
+            return;
+        }
+        
+        if (current === 'videos-plalist') {
+            $rootScope.href = 'videos-plalist';
+            return;
+        }
+        
+        if (current === 'url' && Session.current.url) {
+            $rootScope.href = 'url';
+            $rootScope.url = Session.current.url;
+            return;
+        }
+        
+        if (current.indexOf('whiteboard') !== -1) {
+            $rootScope.id = current.split('/')[1];
+            $rootScope.href = 'whiteboard';
+            return;
+        }
+        
+        $rootScope.href = 'home';
+        return;
+    }, true);
+    
+    function grab(toGrab) {
+        var toGrab = angular.copy(Session.read({name: toGrab}));
+        if (toGrab) {
+            toGrab.url = Session.current.url;
+            toGrab.navigation = Session.current.navigation;
+            Session.update(toGrab);
+        }
+    }
+    
+    function logout() {
+        Session.unset();
+    }
+}
+/* global angular */
+
+angular
+    .module('bemyapp-tech-env')
+    .directive('navigation', NavigationDirective);
+
+function NavigationDirective() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/navigation/navigation.html',
+        controller: 'NavigationController',
+        controllerAs: 'navigationCtrl'
+    };
+}
 /* global angular */
 
 angular
@@ -477,6 +477,41 @@ function VideosPlayerDirective() {
         }
     };
 }
+/* global angular */
+
+angular
+    .module('bemyapp-tech-env')
+    .controller('VideosPlaylistController', VideosPlaylistController);
+
+function VideosPlaylistController(Videos) {
+    var ctrl = this;
+    
+    ctrl.videos = Videos.all;
+    
+    ctrl.add = add;
+    
+    // /////////////////////////////
+    
+    function add(videoId) {
+        Videos.create({videoId: videoId});
+    }
+}
+
+/* global angular */
+
+angular
+    .module('bemyapp-tech-env')
+    .directive('videosPlaylist', VideosPlaylistDirective);
+
+function VideosPlaylistDirective() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/videos/playlist/videos-playlist.html',
+        controller : 'VideosPlaylistController',
+        controllerAs: 'videosPlaylistCtrl'
+    };
+}
+
 /* global angular, go */
 
 angular
@@ -817,41 +852,6 @@ function WhiteboardDirective(InitWhiteboard, LoadWhiteboard) {
         }
     };
 }
-/* global angular */
-
-angular
-    .module('bemyapp-tech-env')
-    .controller('VideosPlaylistController', VideosPlaylistController);
-
-function VideosPlaylistController(Videos) {
-    var ctrl = this;
-    
-    ctrl.videos = Videos.all;
-    
-    ctrl.add = add;
-    
-    // /////////////////////////////
-    
-    function add(videoId) {
-        Videos.create({videoId: videoId});
-    }
-}
-
-/* global angular */
-
-angular
-    .module('bemyapp-tech-env')
-    .directive('videosPlaylist', VideosPlaylistDirective);
-
-function VideosPlaylistDirective() {
-    return {
-        restrict: 'E',
-        templateUrl: 'app/videos/playlist/videos-playlist.html',
-        controller : 'VideosPlaylistController',
-        controllerAs: 'videosPlaylistCtrl'
-    };
-}
-
 /* global angular */
 
 angular
