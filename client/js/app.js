d5d95ee39ed1689d5888361c27a4c603337ef089
Josselin TD <josselin.td@gmail.com>
2016-06-22 23:11:11 +0000

    Add style to navigation

--- client/js/app.js
+++ client/js/app.js
@@ -40,89 +40,6 @@ function ConferenceDirective() {
 
 angular
     .module('bemyapp-tech-env')
-    .controller('SessionController', SessionController);
-
-function SessionController(Session) {
-    var ctrl = this;
-    
-    ctrl.sessions = Session.all;
-    ctrl.hasSession = Session.hasSession;
-    ctrl.current = Session.current;
-    ctrl.selectSession = Session.set;
-}
-
-/* global angular */
-
-angular
-    .module('bemyapp-tech-env')
-    .directive('session', SessionDirective);
-
-function SessionDirective() {
-    return {
-        restrict: 'E',
-        templateUrl: 'app/session/session.html',
-        controller: 'SessionController',
-        controllerAs: 'sessionCtrl'
-    };
-}
-
-/* global angular, localStorage */
-
-angular
-    .module('bemyapp-tech-env')
-    .service('Session', SessionService);
-
-function SessionService(Socket, $rootScope) {
-    var service = this;
-    angular.extend(service, new Socket('session'));
-    
-    service.current = {};
-    
-    service.set = setCurrent;
-    service.unset = unsetCurrent;
-    service.hasSession = hasSession;
-    service.goTo = goTo;
-    
-    $rootScope.$on('readed:session', readed);
-    
-    // /////////////////////////////////////////
-    
-    function setCurrent(name) {
-        var exist = service.read({name: name});
-        if (exist) {
-            localStorage.setItem('session', name);
-            return service.current = exist;
-        }
-        service.create({name: name});
-    }
-    
-    function unsetCurrent() {
-        localStorage.removeItem('session');
-        service.current = {};
-    }
-    
-    function hasSession() {
-        return !!service.current.name;
-    }
-    
-    function goTo(navigation) {
-        if (service.hasSession()) {
-            service.update(angular.extend({}, service.current, {navigation: navigation}));
-        }
-    }
-    
-    function readed() {
-        var storedSession = localStorage.getItem('session');
-        if (storedSession) {
-            setCurrent(storedSession);
-        }
-    }
-}
-
-/* global angular */
-
-angular
-    .module('bemyapp-tech-env')
     .controller('NavigationController', NavigationController);
 
 function NavigationController($scope, $rootScope, Session) {
@@ -218,6 +135,89 @@ function NavigationDirective() {
         controllerAs: 'navigationCtrl'
     };
 }
+/* global angular */
+
+angular
+    .module('bemyapp-tech-env')
+    .controller('SessionController', SessionController);
+
+function SessionController(Session) {
+    var ctrl = this;
+    
+    ctrl.sessions = Session.all;
+    ctrl.hasSession = Session.hasSession;
+    ctrl.current = Session.current;
+    ctrl.selectSession = Session.set;
+}
+
+/* global angular */
+
+angular
+    .module('bemyapp-tech-env')
+    .directive('session', SessionDirective);
+
+function SessionDirective() {
+    return {
+        restrict: 'E',
+        templateUrl: 'app/session/session.html',
+        controller: 'SessionController',
+        controllerAs: 'sessionCtrl'
+    };
+}
+
+/* global angular, localStorage */
+
+angular
+    .module('bemyapp-tech-env')
+    .service('Session', SessionService);
+
+function SessionService(Socket, $rootScope) {
+    var service = this;
+    angular.extend(service, new Socket('session'));
+    
+    service.current = {};
+    
+    service.set = setCurrent;
+    service.unset = unsetCurrent;
+    service.hasSession = hasSession;
+    service.goTo = goTo;
+    
+    $rootScope.$on('readed:session', readed);
+    
+    // /////////////////////////////////////////
+    
+    function setCurrent(name) {
+        var exist = service.read({name: name});
+        if (exist) {
+            localStorage.setItem('session', name);
+            return service.current = exist;
+        }
+        service.create({name: name});
+    }
+    
+    function unsetCurrent() {
+        localStorage.removeItem('session');
+        service.current = {};
+    }
+    
+    function hasSession() {
+        return !!service.current.name;
+    }
+    
+    function goTo(navigation) {
+        if (service.hasSession()) {
+            service.update(angular.extend({}, service.current, {navigation: navigation}));
+        }
+    }
+    
+    function readed() {
+        var storedSession = localStorage.getItem('session');
+        if (storedSession) {
+            setCurrent(storedSession);
+        }
+    }
+}
+
 /* global angular */
 
 angular
